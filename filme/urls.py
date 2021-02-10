9576d50c1b18ba691e2740571aff5b32d6ac09aa
helter daniel <helterdaniel@hotmail.com>
2021-02-10 18:28:59 -0300

    finish crud

--- filme/urls.py
+++ filme/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.FilmeListView.as_view(), name="index"),
     path("post/", views.movie_post, name="post"),
+    path("edit/<int:pk>/", views.edit, name="edit"),
+    path("delete/<int:pk>/", views.delete, name="delete"),
 ]
