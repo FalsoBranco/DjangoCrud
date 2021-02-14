bccd447a8ce3c1dbb5055eabec40264865a3aff5
helter daniel <helterdaniel@hotmail.com>
2021-02-14 18:04:04 -0300

    separando pastas

--- filme/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from filme.models import Filme, Genero
-
-# Register your models here.
-
-
-admin.site.register(Filme)
-admin.site.register(Genero)
--- filme/form.py
+++
@@ -1,9 +0,0 @@
-from django.forms.models import ModelForm
-
-from .models import Filme
-
-
-class FilmeForm(ModelForm):
-    class Meta:
-        model = Filme
-        fields = "__all__"
